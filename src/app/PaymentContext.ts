70accc725f6f131778a36279e0ee23e5891091f7
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-07-07 09:54:03 +0200

    Update PaymentContext.ts

--- src/app/PaymentContext.ts
+++ src/app/PaymentContext.ts
@@ -3,4 +3,18 @@ import { PaymentProvider } from "../core/PaymentProvider";
 
 export class PaymentContext {
   // TODO: Implement the PaymentContext class
+  private provider: PaymentProvider;
+
+  constructor(factory: PaymentProviderFactory) {
+    this.provider = factory.createPaymentProvider();
+  }
+
+  processPayment(amount: number, transactionId: string): void {
+    // 1. Authorize amount
+    this.provider.authorize(amount);
+    // 2. Capture funds
+    this.provider.capture(transactionId);
+    // 3. Refund if needed (simulated)
+    this.provider.refund(transactionId);
+  }
 }
